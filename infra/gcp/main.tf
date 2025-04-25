71d90dd48612125a4aeb3419331928b841f1493e
larsjacobson <76034250+larsjacobson@users.noreply.github.com>
2025-04-25 19:56:31 +0000

    MainTFUpdate

--- infra/gcp/main.tf
+++ infra/gcp/main.tf
@@ -1,36 +1,20 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
 resource "google_project_service" "enabled_services" {
   for_each = toset([
-    "container.googleapis.com",        # GKE
-    "firebase.googleapis.com",         # Firebase
-    "firestore.googleapis.com",        # Firestore
-    "pubsub.googleapis.com",           # Pub/Sub
-    "aiplatform.googleapis.com"        # Vertex AI
+    "container.googleapis.com",
+    "firebase.googleapis.com",
+    "firestore.googleapis.com",
+    "pubsub.googleapis.com",
+    "aiplatform.googleapis.com"
   ])
   service = each.value
-
   disable_on_destroy = true
 }
 
 resource "google_storage_bucket" "terraform_state" {
-  name     = "claritas-tf-state"
-  location = var.region
-  force_destroy = true
-
-  storage_class = "STANDARD"
+  name                        = "claritas-tf-state"
+  location                    = var.region
+  force_destroy               = true
+  storage_class               = "STANDARD"
   uniform_bucket_level_access = true
 
   versioning {
