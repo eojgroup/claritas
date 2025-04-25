985d87bbfa67f41cbfb2459e0d167853ae80cfd7
larsjacobson <76034250+larsjacobson@users.noreply.github.com>
2025-04-25 19:34:49 +0000

    chore(terraform): remove google_credentials and enable full OIDC auth

--- infra/gcp/terraform.tfvars
+++ infra/gcp/terraform.tfvars
@@ -1,1 +1,2 @@
 project_id = "claritas-457808"
+region     = "europe-west1"
--- infra/gcp/variables.tf
+++ infra/gcp/variables.tf
@@ -2,11 +2,6 @@ variable "project_id" {
   type = string
 }
 
-variable "google_credentials" {
-  type        = string
-  description = "Raw JSON for Terraform service account"
-}
-
 variable "region" {
   type    = string
   default = "europe-west1"
