9f25e09ca8d437c3b8e9c404e1a5661dd90522b2
larsjacobson <76034250+larsjacobson@users.noreply.github.com>
2025-04-26 09:34:36 +0000

    tryagain

--- infra/gcp/terraform/variables.tf
+++ infra/gcp/terraform/variables.tf
@@ -1,13 +1,3 @@
 variable "project_id" {
   type = string
 }
-
-variable "region" {
-  type    = string
-  default = "europe-west1"
-}
-
-variable "zone" {
-  type    = string
-  default = "europe-west1-b"
-}
