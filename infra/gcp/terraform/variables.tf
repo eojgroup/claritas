a2b5465bb839bbf34a8e68c413027d25db4f165b
larsjacobson <76034250+larsjacobson@users.noreply.github.com>
2025-04-26 09:43:06 +0000

    tryagain

--- infra/gcp/terraform/variables.tf
+++ infra/gcp/terraform/variables.tf
@@ -1,3 +1,15 @@
 variable "project_id" {
   type = string
 }
+
+variable "region" {
+  description = "The region to deploy resources"
+  type        = string
+  default     = "europe-west1"  # Default to US if not specified
+}
+
+variable "zone" {
+  description = "The zone to deploy resources"
+  type        = string
+  default     = "europe-west1"  # Default to a specific zone in US
+}
