ec5eed95741e9fcfc1c5926a753e5200dcf65d98
larsjacobson <76034250+larsjacobson@users.noreply.github.com>
2025-04-26 07:56:04 +0000

    modify main.tf

--- infra/gcp/terraform/main.tf
+++ infra/gcp/terraform/main.tf
@@ -41,27 +41,6 @@ resource "google_container_cluster" "primary" {
   lifecycle {
     prevent_destroy = true
   }
-
-  labels = {
-    environment = "dev"
-    purpose     = "kubernetes-cluster"
-  }
-}
-
-resource "google_container_cluster" "primary" {
-  name     = "claritas-cluster"
-  location = "us-central1-a"
-
-  initial_node_count = 1
-
-  node_config {
-    machine_type = "e2-small"  # Cheapest option (adjust based on your needs)
-    preemptible  = true         # Preemptible nodes save costs
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 output "kubernetes_cluster_name" {
