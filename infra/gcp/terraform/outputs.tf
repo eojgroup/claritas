8c5e768667b28325e8e3842028c88fd736c4a9e3
larsjacobson <76034250+larsjacobson@users.noreply.github.com>
2025-04-26 10:18:08 +0000

    updateOutputs.tf

--- infra/gcp/terraform/outputs.tf
+++ infra/gcp/terraform/outputs.tf
@@ -1,13 +1,7 @@
-provider "google" {
-  project     = var.project_id
-  region      = var.region  # Updated to use the region variable for consistency
-  zone        = var.zone    # Use zone as required, will default to "us-central1-a"
+output "kubernetes_cluster_name" {
+  value = google_container_cluster.primary.name
 }
 
-provider "kubernetes" {
-  host                   = google_container_cluster.primary.endpoint
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.cluster_ca_certificate)
-  token                  = data.google_client_config.default.access_token
+output "project_id" {
+  value = var.project_id
 }
-
-data "google_client_config" "default" {}
