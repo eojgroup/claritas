01d20b869b9d10d166679ee574afb3f96bee10c8
larsjacobson <76034250+larsjacobson@users.noreply.github.com>
2025-08-26 09:30:34 +0000

    re-structure tf for cloud sql

--- infra/gcp/sql/variables.tf
+++ infra/gcp/sql/variables.tf
@@ -1,8 +1,39 @@
-variable "project_id"       { type = string }
-variable "region"           { type = string  default = "europe-west2" }
-variable "network"          { type = string  description = "VPC self_link (or name if default)" default = "default" }
-variable "db_instance_name" { type = string  default = "claritas-sql" }
-variable "db_name"          { type = string  default = "claritas" }
-variable "db_user"          { type = string  default = "claritas_app" }
-variable "db_tier"          { type = string  default = "db-f1-micro" } # start tiny; change later
-variable "availability_type"{ type = string  default = "ZONAL" }       # "REGIONAL" for HA
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "europe-west2"
+}
+
+variable "network" {
+  type        = string
+  description = "VPC self_link (or name if default)"
+  default     = "default"
+}
+
+variable "db_instance_name" {
+  type    = string
+  default = "claritas-sql"
+}
+
+variable "db_name" {
+  type    = string
+  default = "claritas"
+}
+
+variable "db_user" {
+  type    = string
+  default = "claritas_app"
+}
+
+variable "db_tier" {
+  type    = string
+  default = "db-f1-micro"
+}
+
+variable "availability_type" {
+  type    = string
+  default = "ZONAL" # Use "REGIONAL" for HA
+}
