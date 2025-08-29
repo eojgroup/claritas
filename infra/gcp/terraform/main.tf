c0595e1500d508d90e25c13c09672ca82868c3d7
larsjacobson <76034250+larsjacobson@users.noreply.github.com>
2025-08-29 17:10:39 +0000

    retry

--- infra/gcp/terraform/main.tf
+++ infra/gcp/terraform/main.tf
@@ -7,10 +7,11 @@ resource "google_project_service" "enabled_services" {
     "artifactregistry.googleapis.com",
     "servicenetworking.googleapis.com",
     "sqladmin.googleapis.com",
+    "pubsub.googleapis.com",        
   ])
   project            = var.project_id
   service            = each.value
-  disable_on_destroy = false
+  disable_on_destroy = true
 }
 
 ############################################
