8e1a48110e3a4a58bbe823da4e7693408bf4939c
larsjacobson <76034250+larsjacobson@users.noreply.github.com>
2026-01-29 11:02:59 +0000

    lets try this

--- infra/gcp/terraform/provider.tf
+++ infra/gcp/terraform/provider.tf
@@ -6,7 +6,7 @@ provider "google" {
 
 provider "kubernetes" {
   host                   = google_container_cluster.primary.endpoint
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.cluster_ca_certificate)
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
   token                  = data.google_client_config.default.access_token
 }
 
